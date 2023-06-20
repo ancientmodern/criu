0ab4c0ad2f3739217a37b99551d35742929d864d
Haorong Lu <haorong@rivosinc.com>
2023-06-19 23:19:57 -0700

    criu/arch: copy riscv64 files from aarch64

--- compel/test/infect/victim.c
+++ compel/test/infect/victim.c
@@ -25,10 +25,17 @@ int main(int argc, char **argv)
 	
 		gettimeofday(&tv, NULL);
 		fprintf(stderr, "\033[1;31m calling READ at time Seconds: %ld Microseconds: %ld\n\033[0m", tv.tv_sec, tv.tv_usec);
+
+		if (count == 3) {
+			fprintf(stderr, "\033[1;31m Victim is sleeping 1 sec when count == 3\n\033[0m");
+			// sleep(1);
+			fflush(stderr);
+		}
+
 		num_bytes = read(0, &i, sizeof(i));
 		fprintf(stderr, "\033[1;31m READ value is: %d size is: %ld num_bytes is: %ld\n\033[0m", i, sizeof(i), num_bytes);
 		if (num_bytes != sizeof(i)){
-			fprintf(stderr, "\033[1;31m after READ, ERROR is: %s\n\033[0m", strerror(errno));
+			fprintf(stderr, "\033[1;31m after READ, ERROR is: %s, errno is: %d\n\033[0m", strerror(errno), errno);
 			break;
 		}
 		fprintf(stderr, "\033[1;31m I'm in the MIDDLE of the while loop!\n\033[0m");
@@ -40,6 +47,7 @@ int main(int argc, char **argv)
 		}
 			
 		fprintf(stderr,"\033[1;31m I'm at the END of the while loop!\n\033[0m");	
+		fprintf(stderr,"\033[1;31m ===========================================================================================================\n\033[0m");
 	}
 
 	fprintf(stderr,"\033[1;31m I'm at OUTSIDE of the while loop!\n\033[0m");	
